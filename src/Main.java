d1c1a78698f6a12afdbf4bd1a1de40b252c49a99
Valeriya <tmcxezsim5nn@mail.ru>
2021-09-10 15:00:21 +0300

    src/ modified

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int initialAccount = 100;
+        int deposit = 1100;
+        int bonus;
+        int total;
+
+        if (deposit < 1000) {
+            bonus = 0;
+            total = initialAccount + deposit + bonus;
+            System.out.println("Общий баланс:" + total);
+            System.out.println("Бонус:" + bonus);
+        } else {
+            bonus = deposit / 100;
+            total = initialAccount + deposit + bonus;
+            System.out.println("Общий баланс:" + total);
+            System.out.println("Бонус:" + bonus);
+        }
+        }
+    }
